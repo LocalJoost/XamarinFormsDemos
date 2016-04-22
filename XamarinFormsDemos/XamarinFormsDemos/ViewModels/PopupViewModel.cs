dd3ad295780ffa0bf02c42d68f639276b6cf4d73
Joost van Schaik <joostvanschaik@outlook.com>
2016-04-22 14:47:00 +0200

    Minor bugfix and layout changes

--- XamarinFormsDemos/XamarinFormsDemos/ViewModels/PopupViewModel.cs
+++ XamarinFormsDemos/XamarinFormsDemos/ViewModels/PopupViewModel.cs
@@ -1,9 +1,14 @@
-﻿namespace XamarinFormsDemos.ViewModels
+﻿using Xamarin.Forms;
+
+namespace XamarinFormsDemos.ViewModels
 {
   public class PopupViewModel : MenuViewModelBase
   {
+    public Command CloseMenuCommand { get; private set; }
+
     public PopupViewModel() : base()
     {
+      CloseMenuCommand = new Command(() => IsMenuVisible = false);
     }
   }
 }
