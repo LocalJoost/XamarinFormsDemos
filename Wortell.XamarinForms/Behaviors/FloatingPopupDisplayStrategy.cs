4ae697560b01681512d50be0242e370c7b4636d9
Joost van Schaik <joost.van.schaik@wortell.nl>
2016-06-03 08:05:59 +0200

    Improved code reuse

---
+++ Wortell.XamarinForms/Behaviors/FloatingPopupDisplayStrategy.cs
@@ -0,0 +1,62 @@
+﻿using System.Threading.Tasks;
+using Wortell.XamarinForms.Extensions;
+using Xamarin.Forms;
+
+namespace Wortell.XamarinForms.Behaviors
+{
+  public class FloatingPopupDisplayStrategy
+  {
+    private readonly Label _infoText;
+    private readonly View _overallView;
+    private readonly View _messageView;
+
+    public FloatingPopupDisplayStrategy(Label infoText, View overallView, View messageView)
+    {
+      _infoText = infoText;
+      _overallView = overallView;
+      _messageView = messageView;
+
+      _overallView.GestureRecognizers.Add(new TapGestureRecognizer { Command = new Command(ResetControl) });
+      _overallView.SizeChanged += (sender, args) => { ResetControl(); };
+    }
+
+    public virtual async Task ShowMessageFor(
+      VisualElement parentElement, string text, Point? delta = null)
+    {
+      _infoText.Text = text;
+      _overallView.IsVisible = true;
+
+      // IOS apparently needs to have some time to layout the grid first
+      if (Device.OS == TargetPlatform.iOS) await Task.Delay(25);
+      var gridLocation = _messageView.GetAbsoluteLocation();
+      var parentLocation = parentElement.GetAbsoluteLocation();
+
+      _messageView.TranslationX = parentLocation.X - gridLocation.X -
+                                 _messageView.Width + parentElement.Width +
+                                 delta?.X ?? 0;
+      _messageView.TranslationY = parentLocation.Y - gridLocation.Y +
+                                 parentElement.Height + delta?.Y ?? 0;
+
+      _messageView.Scale = 0;
+      _messageView.Opacity = 1;
+      ExecuteAnimation(0, 1, 250);
+    }
+
+    private void ExecuteAnimation(double start, double end, uint runningTime)
+    {
+      var animation = new Animation(
+        d => _messageView.Scale = d, start, end, Easing.SpringOut);
+
+      animation.Commit(_messageView, "Unfold", length: runningTime);
+    }
+
+    private void ResetControl()
+    {
+      if (_messageView.Opacity != 0)
+      {
+        _messageView.Opacity = 0;
+        _overallView.IsVisible = false;
+      }
+    }
+  }
+}
--- XamarinFormsDemos/XamarinFormsDemos/Views/Controls/FloatingPopupControl.xaml.cs
+++ XamarinFormsDemos/XamarinFormsDemos/Views/Controls/FloatingPopupControl.xaml.cs
@@ -1,47 +1,22 @@
-﻿using System.Diagnostics;
-using System.Threading.Tasks;
+﻿using Wortell.XamarinForms.Behaviors;
 using Wortell.XamarinForms.Controls;
-using Wortell.XamarinForms.Extensions;
 using Xamarin.Forms;
 
 namespace XamarinFormsDemos.Views.Controls
 {
   public partial class FloatingPopupControl : IFloatingPopup
   {
+    private readonly FloatingPopupDisplayStrategy _strategy;
+
     public FloatingPopupControl()
     {
       InitializeComponent();
-      IsVisible = false;
-      MessageGrid.Opacity = 0;
-
-      GestureRecognizers.Add(new TapGestureRecognizer { Command = new Command(ResetControl) });
-      SizeChanged += (sender, args) => { ResetControl(); };
+      _strategy = new FloatingPopupDisplayStrategy(InfoText, this, MessageGrid);
     }
 
-    public async void  ShowMessageFor(VisualElement parentElement, string text, Point? delta = null)
+    public async void ShowMessageFor(VisualElement parentElement, string text, Point? delta = null)
     {
-      InfoText.Text = text;
-      IsVisible = true;
-
-      // IOS apparently needs to have some time to layout the grid first
-      if( Device.OS == TargetPlatform.iOS) await Task.Delay(25);
-      var gridLocation = MessageGrid.GetAbsoluteLocation();
-      var parentLocation = parentElement.GetAbsoluteLocation();
-
-      MessageGrid.TranslationX = parentLocation.X - gridLocation.X - MessageGrid.Width + parentElement.Width +
-                                 delta?.X ?? 0;
-      MessageGrid.TranslationY = parentLocation.Y - gridLocation.Y + parentElement.Height + delta?.Y ?? 0;
-      Debug.WriteLine(MessageGrid.TranslationX + "," + MessageGrid.TranslationY);
-      MessageGrid.Opacity = 1;
-    }
-
-    private void ResetControl()
-    {
-      if (MessageGrid.Opacity != 0)
-      {
-        MessageGrid.Opacity = 0;
-        IsVisible = false;
-      }
+      await _strategy.ShowMessageFor(parentElement, text, delta);
     }
   }
 }
